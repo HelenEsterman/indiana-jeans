c7a26e71f8fd797018f6e7fa70b0fe38af72ab05
Helen Esterman <helenesterman99@gmail.com>
2023-08-22 16:43:30 -0400

    explore chapters

--- scripts/UrbanDweller.js
+++ scripts/UrbanDweller.js
@@ -1,21 +1,23 @@
-import { setSocioLocation } from "./TransientState.js"
+import { setSocioLocation } from "./TransientState.js";
 
-export const LocationChoices = async () =>{
-    const response = await fetch("http://localhost:8088/socioLocations")
-    const locations = await response.json()
+export const LocationChoices = async () => {
+  const response = await fetch("http://localhost:8088/socioLocations");
+  const locations = await response.json();
 
-    document.addEventListener("change", handleLocationChange)
-    
-    let locationHtml = "<h3>Which type of area do you live in?</h3>"
-    for (const location of locations) {
-        locationHtml += `<input type="radio" name="location" value="${location.id}"/>${location.label}`
-    }
-    return locationHtml
-}
+  document.addEventListener("change", handleLocationChange);
+
+  let locationHtml = "<h3>Which type of area do you live in?</h3>";
+  const locationArray = locations.map((location) => {
+    return `<input type="radio" name="location" value="${location.id}"/>${location.label}`;
+  });
+  const locationOptions = locationArray.join(" ");
+  locationHtml += locationOptions;
+  return locationHtml;
+};
 
 const handleLocationChange = (changeEvent) => {
-    if (changeEvent.target.name === "location") {
-        const convertedToInteger = parseInt(changeEvent.target.value)
-        setSocioLocation(convertedToInteger)
-    }
-}
+  if (changeEvent.target.name === "location") {
+    const convertedToInteger = parseInt(changeEvent.target.value);
+    setSocioLocation(convertedToInteger);
+  }
+};
