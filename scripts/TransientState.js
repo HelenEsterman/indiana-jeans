3dc9500f09b6d76a59d5fb90463580a1b07dd321
Helen Esterman <helenesterman99@gmail.com>
2023-08-23 12:53:01 -0400

    IN THE MIDDLE of substituting the transientState object for a Map

--- scripts/TransientState.js
+++ scripts/TransientState.js
@@ -1,35 +1,38 @@
 // Set up the transient state data structure and provide initial valuess
-const transientState = {
-    "ownsBlueJeans": false,
-    "socioLocationId": 0
-}
-
+// const transientState = {
+//     "ownsBlueJeans": false,
+//     "socioLocationId": 0
+// }
+const transientState = new Map();
+transientState.set("ownsBlueJeans", false);
+transientState.set("socioLocationId", 0);
 
 // Functions to modify each property of transient state
 export const setOwnsBlueJeans = (selectedChoice) => {
-    transientState.ownsBlueJeans = selectedChoice
-    // console.log(transientState)
-}
+  transientState.set("ownsBlueJeans", selectedChoice);
+  //   console.log(transientState);
+};
 
 export const setSocioLocation = (selectedChoice) => {
-    transientState.socioLocationId = selectedChoice
-    // console.log(transientState)
-}
-
-
+  transientState.set("socioLocationId", selectedChoice);
+  //   console.log(transientState);
+};
 
 // Function to convert transient state to permanent state
 
-export const saveSurveySubmission = async() => {
-    const postOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(transientState)
-}
-    const response = await fetch("http://localhost:8088/submissions", postOptions)
+export const saveSurveySubmission = async () => {
+  const postOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(transientState),
+  };
+  const response = await fetch(
+    "http://localhost:8088/submissions",
+    postOptions
+  );
 
-    const customEvent = new CustomEvent("newSubmissionCreated")
-    document.dispatchEvent(customEvent)
-}
+  const customEvent = new CustomEvent("newSubmissionCreated");
+  document.dispatchEvent(customEvent);
+};
